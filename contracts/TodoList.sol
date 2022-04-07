78bb7448793071837e2cd6c932dc4c0b6de55b9d
NeginAryapour <negin_aryapour@hotmail.com>
2022-04-07 14:27:36 +0430

    add list of tasks to smart contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,24 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public tasksCount = 0;
+
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public{
+        createTask("check out dappunivercity.com");
+    }
+
+    function createTask(string memory _content) public {
+        tasksCount ++;
+        tasks[tasksCount] = Task(tasksCount, _content, false);
+
+    }
+
+    
 }
